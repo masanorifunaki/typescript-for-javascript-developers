afee23307734023daba880dabc702c702ad481bf
舟木正憲 <funakimfunaki@gmail.com>
2020-02-11 09:39:06 +0900

    getter と setter

---
+++ src/getter-and-setter.ts
@@ -0,0 +1,23 @@
+export {};
+
+class MyNumberCard {
+  private _owner: string;
+  private _secretNumber: number;
+
+  constructor(owner: string, secretNumber: number) {
+    this._owner = owner;
+    this._secretNumber = secretNumber;
+  }
+
+  get owner() {
+    return this._owner;
+  }
+
+  set secretNumber(secretNumber: number) {
+    this._secretNumber = secretNumber;
+  }
+}
+
+let card = new MyNumberCard('Taro', 10000);
+console.log(card.owner);
+card.secretNumber = 111111;
