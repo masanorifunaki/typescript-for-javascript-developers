5fa616be32923f784343bcd5c2c35e0a6814135f
舟木正憲 <funakimfunaki@gmail.com>
2020-02-11 12:53:53 +0900

    constアサーション

---
+++ src/as-const.ts
@@ -0,0 +1,13 @@
+export {};
+
+let name = 'Taro';
+
+let nickName = 'Yamada' as const;
+// nickName = 'A';
+
+let profile = {
+  name: 'Yamda',
+  height: 178,
+} as const;
+
+// profile.name = 'Taro';
