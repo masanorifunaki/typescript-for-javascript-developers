c5de75bf72b3be14000aa81633841a33c35400cd
舟木正憲 <funakimfunaki@gmail.com>
2020-02-11 10:47:24 +0900

    継承

---
+++ src/inheritance.ts
@@ -0,0 +1,30 @@
+export {};
+
+class Animal {
+  constructor(public name: string) {
+  }
+
+  run(): string {
+    return 'I can run';
+  }
+}
+
+class Lion extends Animal {
+  public speed: number;
+
+  constructor(name: string, speed: number) {
+    super(name);
+
+    this.speed = speed;
+  }
+
+  run(): string {
+    return `${ super.run() } ${ this.speed }km.`;
+  }
+}
+
+// let animal = new Animal();
+// console.log(animal.run());
+
+let lion = new Lion('Lion', 80);
+console.log(lion.run());
